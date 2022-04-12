ef234aa1bb3a2bedbb3004505befd0a3c6d58a8a
AhamedJINabil <johayeriqbal@gmail.com>
2022-04-13 05:19:31 +0600

    user signin signup complete

--- src/__auth_steps.js
+++ src/__auth_steps.js
@@ -1,11 +1,15 @@
 /**
- * 1. create a new project in console.firebase.google.com
+ * 1. create a new firebase project in console.firebase.google.com
  * 2. npm install firebase
  * 3. create firebase.init.js and pest firebase initialize code in it.
  * 4. in firebase.init.js import getAuth form firebase/auth and pass app as parameter then export default auth.
  *  5. firebase setting > authentication > enable auth provider (email-password, google, facebook, github, etc)
  * 6. create login, signup component setup route   
  * 7. attach form field handler and form submit handler.
+ * 8. install firebase hooks ( npm install --save react-firebase-hooks )
+ * 9. useCreateUserWithEmailAndPassword form react-firebase-hooks.
+ * 10. if user is created redirect to the expected page
+ * 11. useSignInWithEmailAndPassword for sign in.
 */ 
 
 
